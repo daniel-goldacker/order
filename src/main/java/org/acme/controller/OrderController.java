d707752fa16213e3ced87963a7dd5cef12e8d498
Daniel Goldacker <daniel-goldacker@hotmail.com>
2025-09-03 13:23:32 -0300

    ajuste no projeto para usar o keycloak do autenticacao a nivel de roles para chamada das apis

--- src/main/java/org/acme/controller/OrderController.java
+++ src/main/java/org/acme/controller/OrderController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.acme.dto.OrderDTO;
 import org.acme.service.OrderService;
 
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
@@ -21,6 +22,7 @@ public class OrderController {
     private OrderService orderService;
 
     @GET
+    @RolesAllowed({"user", "admin"})
     @Produces(MediaType.APPLICATION_JSON)
     public List<OrderDTO> getAllOrders(){
         return orderService.getAllOrders();
